fbed72c2330fafa26fa1ed1abcf23ec955fc3a38
tommaso <tommaso.doninelli@my-ideas.it>
2016-06-17 03:24:26 +0200

    Code refactoring

--- drawer.py
+++ drawer.py
@@ -7,62 +7,46 @@ from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import pandas as pd
 import numpy as np
 from plotly.tools import FigureFactory as FF
+import glob
+import os
+
+
 
 plotly.tools.set_credentials_file(username='totomz', api_key='uk5clj1ogp')
 
 def testCsv():
-    # Import data from csv
-
-    # For each dataset (file)
-    df = pd.read_csv('medie.csv', header=0, decimal='.')
 
     # For each file
     #   For each column
     #       do Heatmap
     #       do Contour
-    col = [c for c in df.columns.tolist() if c[0].islower() ]
 
-    # PEr ogni file
-        # Per ogni colonna
-            # Faccio heatmap e contour che si chiamano file - colonna
+    for csv in [f for f in os.listdir('./') if f.endswith("csv")]:
 
+        print("Reading " + csv)
 
-    print(df)
-    print("**********************")
-    print(df.values)
-    print("---")
-    print(df.size)
-    print("**********************")
+        df = pd.read_csv(csv, header=0, decimal='.')
 
-#    data = [go.Heatmap(x=df['Colonna'],
-#                       y=df['Riga'],
-#                       z=df['distance_kpOU'].tolist(),
-#                       reversescale=True)]
-    data = [go.Contour(x=df['Colonna'],
-                       y=df['Riga'],
-                       z=df['distance_kpOU'].tolist(),
-                       reversescale=True)]
+        csv = csv.split('.')[0]         # Keep only filename
+        # data columns are lowercase
+        for col in [c for c in df.columns.tolist() if c[0].islower()]:
+                dataheat = [go.Heatmap(x=df['Colonna'],
+                                       y=df['Riga'],
+                                       z=df[col].tolist(),
+                                       reversescale=True)]
+                datacont = [go.Contour(x=df['Colonna'],
+                                       y=df['Riga'],
+                                       z=df[col].tolist(),
+                                       reversescale=True)]
 
-    layout = go.Layout(title='distance_kpOU')
+                title = csv + '-' + col
+                py.image.save_as(go.Figure(data=dataheat, layout=go.Layout(title=title)),
+                                 title + '-heatmap.png')
 
-    fig = go.Figure(data=data,layout=layout)
+                py.image.save_as(go.Figure(data=datacont, layout=go.Layout(title=title)),
+                                 title + '-contour.png')
 
-
-    # Save the figure as a png image:
-    py.image.save_as(fig, 'mssy_plot.png')
-
-    print("Immagine salvata")
-
-
-def createHeatmap(file, column):
-    data = [
-        go.Heatmap(
-            z=[[1, 20, 30],
-               [20, 1, 60],
-               [30, 60, 1]]
-        )
-    ]
-    plot(data, filename='basic-heatmap')
+                print("   Done!")
 
 def main():
     testCsv()
