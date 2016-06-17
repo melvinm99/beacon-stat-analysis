d490661e94938c8287eabb9fc2b40ec3b665aec5
tommaso <tommaso.doninelli@my-ideas.it>
2016-06-17 03:34:22 +0200

    Ok but sloooow

--- drawer.py
+++ drawer.py
@@ -3,19 +3,14 @@ __author__ = 'tommaso'
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import pandas as pd
-import numpy as np
-from plotly.tools import FigureFactory as FF
-import glob
 import os
-
 
 
 plotly.tools.set_credentials_file(username='totomz', api_key='uk5clj1ogp')
 
-def testCsv():
 
+def main():
     # For each file
     #   For each column
     #       do Heatmap
@@ -41,15 +36,12 @@ def testCsv():
 
                 title = csv + '-' + col
                 py.image.save_as(go.Figure(data=dataheat, layout=go.Layout(title=title)),
-                                 title + '-heatmap.png')
+                                 'images/' + title + '-heatmap.png')
 
                 py.image.save_as(go.Figure(data=datacont, layout=go.Layout(title=title)),
-                                 title + '-contour.png')
+                                 'images/' + title + '-contour.png')
 
                 print("   Done!")
-
-def main():
-    testCsv()
 
 if __name__ == "__main__":
     main()
